eae25541bbbd8629abfc46a4d353d098be6bfc09
Ayodeji Babatunde <SN027869@firstbanknigeria.com>
2021-07-25 20:59:00 -0500

    commiting lab6

--- js/ascii.js
+++ js/ascii.js
@@ -1,79 +1,82 @@
 window.onload = function(){
-    "use strict";
-    // put all of your code here
+    
+    let btnStart = document.getElementById("start");
+    let btnStop = document.getElementById("stop");
+    let txtArea = document.getElementById("text-area");
+    let cbTurbo = document.getElementById("turbo");
+    let ddlAnimations = document.getElementById("animation");
+    let ddlSizeOfFont = document.getElementById("fontsize");
 
-    //variables
-    let textArea = document.getElementById("text-area");
-    let startBtn = document.getElementById("start");
-    let stopBtn = document.getElementById("stop");
-    let animationSlct = document.getElementById("animation");
-    let fontSizeSlct = document.getElementById("fontsize");
-    let speedChckBx = document.getElementById("turbo");
-
-    let timer = null; // stores ID of interval timer 
-    let frameSeq;
-    let currentFrame = 0;
-    let lastFrame;
+    let timer = null; 
+    let frame1;
+    let frame2 = 0;
+    let frame3;
     let turbo = 250;
     let running;
 
-    //actions
-    startBtn.onclick = start;
-    stopBtn.onclick = stop;
-    animationSlct.onchange = display;
-    fontSizeSlct.onchange = size;
-    speedChckBx.onclick = speed;
+    btnStart.onclick = start;
+    btnStop.onclick = stop;
+    ddlAnimations.onchange = display;
+    ddlSizeOfFont.onchange = size;
+    cbTurbo.onclick = speed;
 
-    //start
+    //starting the animations
     function start() {
-        alert("Starting Anmiation Now");
         running = true;
-        stopBtn.disabled = false;
-        startBtn.disabled = true;
-        textArea.disabled = true;
-        animationSlct.disabled = true;
+        btnStop.disabled = false;
+        btnStart.disabled = true;
+        txtArea.disabled = true;
+        ddlAnimations.disabled = true;
 
-        lastFrame = textArea.value;
+        frame3 = txtArea.value;
         speedCheck();
-        if (textArea.value != " ") {
-            playAnimation2(textArea.value);
+        if (txtArea.value != " ") {
+            playAnimation2(txtArea.value);
         } else {
             playAnimation();
         }
     }
 
-    //stop
+    
     function stop() {
-        stopBtn.disabled = true;
-        startBtn.disabled = false;
-        textArea.disabled = false;
-        animationSlct.disabled = false;
+        btnStop.disabled = true;
+        btnStart.disabled = false;
+        txtArea.disabled = false;
+        ddlAnimations.disabled = false;
 
-        alert("Stopping Anmiation Now");
+        txtArea.value = frame3;
+        frame3 = frame2;
+        running = false;
         clearInterval(timer);
         timer = null;
-        textArea.value = lastFrame;
-        lastFrame = currentFrame;
-        running = false;
     }
 
-    //change animation
     function display() {
-        textArea.value = ANIMATIONS[animationSlct.value.toString()];
+        txtArea.value = ANIMATIONS[ddlAnimations.value.toString()];
     }
 
-    //change size
     function size() {
-        // textArea.classList.add('text-area'); //unobtrusive styling is not dynamic
-        // textArea.style.width = "960px";
-        // textArea.style.height = "360px";
-        textArea.style.fontSize = fontSizeSlct.value;
+        txtArea.style.fontSize = ddlSizeOfFont.value;
     }
 
-    //change speed
+    function playAnimation2(frameStr) {
+        if (frameStr.indexOf("\r\n") != -1) {
+            frame1 = frameStr.split("=====\r\n");
+        } else {
+            frame1 = frameStr.split("=====\n");
+        }
+
+        timer = setInterval(showNextFrame, turbo);
+    }
+
+    function showNextFrame(){
+        txtArea.value = frame1[frame2];
+        frame2 = (frame2 + 1) % frame1.length;
+    }
+
     function speed(){
         if(running){
-            if (speedChckBx.checked == true) {
+            if (cbTurbo.checked == true) {
                 clearInterval(timer);
                 turbo = 50;
                 timer = setInterval(showNextFrame, turbo);
@@ -87,7 +90,7 @@ window.onload = function(){
 
     //speed status
     function speedCheck(){
-        if (speedChckBx.checked == true) {
+        if (cbTurbo.checked == true) {
             turbo = 50;
         } else {
             turbo = 250;
@@ -95,32 +98,13 @@ window.onload = function(){
     }
 
     function playAnimation() {
-
-        let frameStr = ANIMATIONS[animationSlct.value.toString()];
-
+        let frameStr = ANIMATIONS[ddlAnimations.value.toString()];
         if (frameStr.indexOf("\r\n") != -1) {
-            frameSeq = frameStr.split("=====\r\n");
+            frame1 = frameStr.split("===\r\n");
         } else {
-            frameSeq = frameStr.split("=====\n");
+            frame1 = frameStr.split("===\n");
         }
-
         timer = setInterval(showNextFrame, turbo);
-
-    }
-
-    function playAnimation2(frameStr) {
-        if (frameStr.indexOf("\r\n") != -1) {
-            frameSeq = frameStr.split("=====\r\n");
-        } else {
-            frameSeq = frameStr.split("=====\n");
-        }
-
-        timer = setInterval(showNextFrame, turbo);
-    }
-
-    function showNextFrame(){
-        textArea.value = frameSeq[currentFrame];
-        currentFrame = (currentFrame + 1) % frameSeq.length;
     }
 
 };
