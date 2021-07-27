99d713987a5d1b7b318eec35c20a0360224af266
Ayodeji Babatunde <SN027869@firstbanknigeria.com>
2021-07-26 22:40:58 -0500

    commiting lab7

--- js/tests.js
+++ js/tests.js
@@ -25,6 +25,6 @@ describe("reverse", function () {
 describe("filterWord", function () {
     it("Print words that has length more than the integer specified",
         function () {
-            assert.equal(JSON.stringify(["Johnny", "Johnson"]), filterWord(["John", "Johnny", "Johnson"], 4));
+            assert.equal(JSON.stringify(["Johnny", "Johnson"]), filterWord(JSON.stringify(["John", "Johnny", "Johnson"]), 4));
         });
 });
